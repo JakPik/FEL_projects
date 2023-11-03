268ac6c5b2636d0e2ead5415a6ceaaf48086c7dc
JakPik <jakub.pikal@gmail.com>
2023-11-03 22:38:40 +0100

    HW04 funkční základní zadání pro prvočísla 10^6

--- C_kompilace/HW04/main.c
+++ C_kompilace/HW04/main.c
@@ -5,28 +5,28 @@
 #define MULTIPLICATION_SYMBOL "x"
 #define POWER_SYMBOL "^"
 
-long Eratosthen_sieve(long limit, long pole[limit + 1]);
-long Rewrite_array(long limit, long size, long pole[limit + 1],
-                   long prvocisla[size + 1]);
-int input_check(long input, int test, int err);
-void print_line(long input);
-void prime_decomposition_print(long divider, long *number);
-void print_multiplication(long *previous, long *number);
-void prime_number_decomposition(long input, long size, long pole[size + 1]);
-void prime_number_print(long input, long size, long pole[size + 1]);
+int Eratosthen_sieve(int limit, int pole[limit + 1]);
+int Rewrite_array(int limit, int size, int pole[limit + 1],
+int prvocisla[size + 1]);
+int input_check(int input, int test, int err);
+void print_line(int input);
+void prime_decomposition_print(int divider, int *number);
+void print_multiplication(int *previous, int *number);
+void prime_number_decomposition(int input, int size, int pole[size + 1]);
+void prime_number_print(int input, int size, int pole[size + 1]);
 
 int main(void) {
-  long limit = 10000000;
-  long pole[limit + 1];
+    int limit = 1000000;
+    int pole[limit + 1];
   int err = 0;
-  long input = 1;
+    int input = 1;
   int first = 0;
 
-  long size = Eratosthen_sieve(limit, pole);
-  long prvocisla[size + 1];
+    int size = Eratosthen_sieve(limit, pole);
+    int prvocisla[size + 1];
   Rewrite_array(limit, size, pole, prvocisla);
   while (1) {
-    int test = scanf("%ld", &input);
+    int test = scanf("%d", &input);
     if (first == 1)
       printf("\n");
     if (input_check(input, test, err) != 0)
@@ -39,31 +39,31 @@ int main(void) {
   return 0;
 }
 
-long Eratosthen_sieve(long limit, long pole[limit + 1]) {
-  long count = 0;
+int Eratosthen_sieve(int limit, int pole[limit + 1]) {
+    int count = 0;
 
-  for (long i = 0; i < limit; i++) {
+  for (int i = 0; i < limit; i++) {
     pole[i] = 1;
   }
 
-  for (long i = 2; i * i <= limit; i++) {
+  for (int i = 2; i * i <= limit; i++) {
     if (pole[i]) {
-      for (long j = i * i; j <= limit; j += i)
+      for (int j = i * i; j <= limit; j += i)
         pole[j] = 0;
     }
   }
 
-  for (long i = 2; i < limit; i++) {
+  for (int i = 2; i < limit; i++) {
     if (pole[i])
       count += 1;
   }
   return count;
 }
 
-long Rewrite_array(long limit, long size, long pole[limit + 1],
-                   long prvocisla[size + 1]) {
-  long position = 0;
-  for (long i = 2; i <= limit; i++)
+int Rewrite_array(int limit, int size, int pole[limit + 1],
+int prvocisla[size + 1]) {
+    int position = 0;
+  for (int i = 2; i <= limit; i++)
     if (pole[i]) {
       prvocisla[position] = i;
       position += 1;
@@ -71,7 +71,7 @@ long Rewrite_array(long limit, long size, long pole[limit + 1],
   return 0;
 }
 
-int input_check(long input, int test, int err) {
+int input_check(int input, int test, int err) {
   if ((input < 0) || (test == 0)) {
     fprintf(stderr, "Error: Chybny vstup! \n");
     err = ERROR_WRONG_INPUT;
@@ -79,21 +79,21 @@ int input_check(long input, int test, int err) {
   return err;
 }
 
-void print_line(long input) {
-  printf("Prvociselny rozklad cisla %ld je:\n", input);
+void print_line(int input) {
+  printf("Prvociselny rozklad cisla %d je:\n", input);
 }
 
-void prime_decomposition_print(long divider, long *number) {
-  long remaining = *number;
-  long count = 0;
+void prime_decomposition_print(int divider, int *number) {
+    int remaining = *number;
+    int count = 0;
   if (remaining % divider == 0) {
-    printf("%ld", divider);
+    printf("%d", divider);
     while (remaining % divider == 0) {
       count += 1;
       remaining = remaining / divider;
     }
     if (count != 1)
-      printf(POWER_SYMBOL "%ld", count);
+      printf(POWER_SYMBOL "%d", count);
     *number = remaining;
     // return 0;
   }
@@ -101,17 +101,17 @@ void prime_decomposition_print(long divider, long *number) {
   // return 0;
 }
 
-void print_multiplication(long *previous, long *number) {
+void print_multiplication(int *previous, int *number) {
   if (*number != *previous) {
     printf(" " MULTIPLICATION_SYMBOL " ");
     *previous = *number;
   }
 }
 
-void prime_number_decomposition(long input, long size, long pole[size + 1]) {
-  long number = input;
-  long previous = input;
-  for (long i = 0; i < size; i++) {
+void prime_number_decomposition(int input, int size, int pole[size + 1]) {
+    int number = input;
+    int previous = input;
+  for (int i = 0; i < size; i++) {
     prime_decomposition_print(pole[i], &number);
     if (number == 1)
       break;
@@ -120,10 +120,10 @@ void prime_number_decomposition(long input, long size, long pole[size + 1]) {
   }
 }
 
-void prime_number_print(long input, long size, long pole[size + 1]) {
+void prime_number_print(int input, int size, int pole[size + 1]) {
   if (input == 1) {
     print_line(input);
-    printf("%ld", input);
+    printf("%d", input);
   } else {
     print_line(input);
     prime_number_decomposition(input, size, pole);
