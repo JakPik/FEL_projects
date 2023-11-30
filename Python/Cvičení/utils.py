c96b1f9ec4d51c39e917306a8f57e465a0c1bc4e
JakPik <jakub.pikal@gmail.com>
2023-11-30 12:49:27 +0100

    python reader

---
+++ Python/Cvičení/utils.py
@@ -0,0 +1,27 @@
+def read_classification_from_file(filepath):
+    dictonary = {}
+    with open(filepath, 'rt', encoding='utf-8') as f:
+        for _, text in enumerate(f):
+            string, numbers = separate_text_and_number(text)
+            dictonary[string] = numbers
+    return dictonary
+
+def write_classification_to_file(dictonary):
+    with open("result.txt", 'wt', encoding="utf-8") as f:
+        for letters in dictonary:
+            numbers = dictonary[letters]
+            string = f"{letters} ... {numbers}\n"
+            f.write(string)
+
+
+def separate_text_and_number(string):
+    n = ''.join(char for char in string if char.isdigit())
+    l = ''.join(char for char in string if char.isalpha())
+    numbers = ''.join(n)
+    letters = ''.join(l)
+    return letters, numbers
+
+if __name__ == "__main__":
+    dictonary = read_classification_from_file("./Python/Cvičení/clasification.txt")
+    write_classification_to_file(dictonary)
+
